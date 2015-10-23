13d104ac84a22b6b19f0f821f1a2544825ba302d
Karan Vahi <vahi@isi.edu>
2015-10-23 07:50:30 -0700

    helper script to setup the tutorial training accounts.

---
+++ ISI/examples/tutorial_setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+
+set -e 
+
+for count in `seq -w  1 30`; do
+    user=pegtrain$count
+
+    echo "------------------------------"
+    echo "setting up for user $user"
+
+#    set the password
+#    echo "setting password for user $user"
+#    echo "XXX" | passwd --stdin $user
+
+    
+    su - $user <<EOF
+
+	cd ~${user}/
+
+	set +e
+	condor_rm $user
+	set -e
+
+	rm -rf ./examples
+	cp -r /local-scratch/vahi/software/git/tutorials/ISI/examples .
+
+	cd examples
+	./generate_catalogs.sh
+        rm ./*~ ./tutorial_setup.sh
+	cd ..
+	
+	#setup workflow db for dashboard
+	rm -f ~${user}/.pegasus/workflow.db
+	pegasus-db-admin create
+	chmod +r ~${user}/.pegasus/workflow.db
+        chmod +rx ~${user}
+	chmod +rx ~${user}/.pegasus
+
+EOF
+     
+done
