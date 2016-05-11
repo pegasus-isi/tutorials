6a58f731da9d225c10174568464c2098a88276f3
Karan Vahi <vahi@isi.edu>
2016-05-11 15:00:53 -0700

    updates to setup

--- ISI/examples/tutorial_setup.sh
+++ ISI/examples/tutorial_setup.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-for count in `seq -w  1 3`; do
+for count in `seq -w  01 03`; do
     user=pegtrain$count
 
     echo "------------------------------"
@@ -24,6 +24,8 @@ for count in `seq -w  1 3`; do
 	set -e
 
         # setup SSH key to be used by tutorial
+        rm -rf ~/.ssh
+        mkdir -p ~/.ssh
         ssh-keygen -b 2048 -t rsa -f ~/.ssh/workflow -N ""
         cat ~/.ssh/workflow.pub > ~/.ssh/authorized_keys2
 
@@ -33,6 +35,9 @@ for count in `seq -w  1 3`; do
 	chmod +r ~${user}/.pegasus/workflow.db
         chmod +rx ~${user}
 	chmod +rx ~${user}/.pegasus
+
+        rm -rf ~/examples ~/run ~/tutorial
+        mkdir -p ~/tutorial
 
 EOF
      
